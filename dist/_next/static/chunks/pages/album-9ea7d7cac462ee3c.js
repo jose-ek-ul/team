f5d8c7a8a96296ac5ac5551f6260ae5af2a31b1f
jose-ek-ul <142610988+jose-ek-ul@users.noreply.github.com>
2023-10-25 09:02:10 -0600

    initial commit

---
+++ dist/_next/static/chunks/pages/album-9ea7d7cac462ee3c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[818],{4420:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/album",function(){return u(5911)}])}},function(n){n.O(0,[774,888,179],function(){return n(n.s=4420)}),_N_E=n.O()}]);
---
+++ dist/_next/static/chunks/pages/index-e2a668a43eaafc85.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{8312:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return t(2603)}])},2603:function(n,u,t){"use strict";t.r(u);var _=t(5893),e=t(5911);u.default=function(){return(0,_.jsx)(e.default,{})}}},function(n){n.O(0,[774,888,179],function(){return n(n.s=8312)}),_N_E=n.O()}]);
